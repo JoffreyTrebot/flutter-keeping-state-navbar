f2cbaf6bcfde67ce4baeb99ed5c1436d12d5874f
Joffrey <trebotjoffrey@gmail.com>
2020-07-31 00:05:04 +0200

    refactor(bottom_navigation): update bloc implementation

--- hello_world/lib/src/blocs/bottom_navigation/bottom_navigation.bloc.dart
+++ hello_world/lib/src/blocs/bottom_navigation/bottom_navigation.bloc.dart
@@ -1,33 +1,23 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 
-import 'bottom_navigation.event.dart';
-import 'bottom_navigation.state.dart';
+part 'bottom_navigation.event.dart';
+part 'bottom_navigation.state.dart';
 
 class BottomNavigationBloc
     extends Bloc<BottomNavigationEvent, BottomNavigationState> {
-  int currentIndex = 0;
-
-  BottomNavigationBloc() : super(PageLoading());
+  BottomNavigationBloc() : super(HomeState());
 
   @override
   Stream<BottomNavigationState> mapEventToState(
       BottomNavigationEvent event) async* {
-    if (event is AppStarted) {
-      this.add(PageTapped(index: this.currentIndex));
+    if (event is LoadHome) {
+      yield HomeState();
     }
-    if (event is PageTapped) {
-      this.currentIndex = event.index;
-      yield CurrentIndexChanged(currentIndex: this.currentIndex);
-      yield PageLoading();
-
-      if (this.currentIndex == 0) {
-        yield FirstPageLoaded();
-      }
-      if (this.currentIndex == 1) {
-        yield SecondPageLoaded();
-      }
+    if (event is LoadMap) {
+      yield MapState();
     }
   }
 }
--- hello_world/lib/src/blocs/bottom_navigation/bottom_navigation.state.dart
+++ hello_world/lib/src/blocs/bottom_navigation/bottom_navigation.state.dart
@@ -1,38 +1,21 @@
-import 'package:meta/meta.dart';
-import 'package:equatable/equatable.dart';
+part of 'bottom_navigation.bloc.dart';
 
-@immutable
 abstract class BottomNavigationState extends Equatable {
-  BottomNavigationState([List props = const []]);
-
-   @override
-    List<Object> get props => [];
+  const BottomNavigationState();
 }
 
-class CurrentIndexChanged extends BottomNavigationState {
-  final int currentIndex;
-
-  CurrentIndexChanged({@required this.currentIndex}) : super([currentIndex]);
-
-  @override
-  List<Object> get props => [currentIndex];
+class HomeState extends BottomNavigationState {
+  final int index = 0;
+  final String title = 'Home';
 
   @override
-  String toString() => 'CurrentIndexChanged to $currentIndex';
+  List<Object> get props => [index, title];
 }
 
-class PageLoading extends BottomNavigationState {
-  @override
-  String toString() => 'PageLoading';
-}
-
-class FirstPageLoaded extends BottomNavigationState {
+class MapState extends BottomNavigationState {
+  final int index = 1;
+  final String title = 'Map';
 
   @override
-  String toString() => 'FirstPageLoaded';
-}
-
-class SecondPageLoaded extends BottomNavigationState {
-  @override
-  String toString() => 'SecondPageLoaded';
+  List<Object> get props => [index, title];
 }
